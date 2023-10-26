0a44e80b3b20bd77b01c8f4324d8964a56cc5324
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2023-10-26 10:19:42 -0300

    Reparo Gerenciamento

--- Front-end/soturno/src/pages/gerenciamento/gerenciamento.jsx
+++ Front-end/soturno/src/pages/gerenciamento/gerenciamento.jsx
@@ -11,12 +11,12 @@ export default function Gerenciamento() {
                 <Header/>
                 <section className="section-img">
                     <a className="a-gerenciamento" href="/cadastro-poção">
-                        <img src="/assets/images/Produtos/esqueleto-pocao.png"
+                        <img src="/assets/images/produtos/esqueleto-pocao.png"
                             alt="Mão de esqueleto segurando uma placa escrito poção"/>
                     </a>
 
                     <a className="a-gerenciamento" href="/cadastro-feitiço">
-                        <img src="/assets/images/Produtos/esqueleto-feitico.png"
+                        <img src="/assets/images/produtos/esqueleto-feitico.png"
                             alt="Mão de esqueleto segurando uma placa escrito feitiço"/>
                     </a>
 
@@ -33,7 +33,7 @@ export default function Gerenciamento() {
             </section>
 
             <figure className="figure-chao-produtos">
-                <img className="chao-produtos" src="/assets/images/Produtos/chaodeossos-produtos.png"
+                <img className="chao-produtos" src="/assets/images/produtos/chaodeossos-produtos.png"
                     alt="Chão de Ossos"/>
             </figure>
 

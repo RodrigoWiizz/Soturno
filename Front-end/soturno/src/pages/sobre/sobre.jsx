b9424bf1df7f8d00ef183ecd6d3360e1b1089d1a
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2023-10-29 12:46:39 -0300

    Manutenção Responsividade Home/Sobre/Contato/Login

--- Front-end/soturno/src/pages/sobre/sobre.jsx
+++ Front-end/soturno/src/pages/sobre/sobre.jsx
@@ -12,35 +12,35 @@ export default function Sobre() {
                 <Header />
 
                 <main className="main-sobre">
-                    
-                <pre className="pre-p-sobre-descricao-titulo"> {`
+
+                    <pre className="pre-p-sobre-descricao-titulo"> {`
 Bem-vindo à "Soturno: A Loja dos Feitiços", o seu destino mágico para todas as suas necessidades místicas e sobrenaturais! Aqui, exploramos os segredos do oculto e oferecemos uma variedade de produtos mágicos, desde poções de cura até poções de ataque, defesa e feitiços diversos. Entre e descubra um mundo de possibilidades mágicas que irão encantar e proteger você.
 
 `}</pre>
 
-<p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#44FF02' }}>1. Poções de Cura:</span></p>
-<pre className="pre-p-sobre-descricao-titulo"> {`
+                    <p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#44FF02' }}>1. Poções de Cura:</span></p>
+                    <pre className="pre-p-sobre-descricao-titulo"> {`
 Nossa linha de Poções de Cura é perfeita para aqueles que buscam restaurar sua saúde e vitalidade. Temos uma ampla gama de opções, desde poções simples que curam pequenos ferimentos até elixires poderosos que podem reviver até mesmo os mais gravemente feridos. Alguns dos nossos produtos mais populares incluem:
 Poção de Cura Rápida: Ideal para curar cortes e arranhões leves, esta poção é uma escolha favorita dos aventureiros que desejam manter-se em boa forma durante suas jornadas.
 Elixir da Vida: Esta poção lendária é conhecida por trazer os moribundos de volta à vida. Um verdadeiro milagre em um frasco!
 
 `}</pre>
-<p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#00B2FF' }}>2. Poções de Defesa:</span></p>
-<pre className="pre-p-sobre-descricao-titulo"> {`
+                    <p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#00B2FF' }}>2. Poções de Defesa:</span></p>
+                    <pre className="pre-p-sobre-descricao-titulo"> {`
 Nossas Poções de Defesa são essenciais para aqueles que enfrentam perigos constantes e desejam se proteger contra os ataques de criaturas mágicas e adversários formidáveis. Confira algumas das opções disponíveis:
 Poção de Proteção Elemental: Esta poção cria uma barreira protetora que absorve dano mágico de fogo, gelo, eletricidade e outros elementos.
 Elixir da Resistência: Reforça sua resistência física, tornando-o mais resistente a ferimentos e impactos.
 
 `}</pre>
-<p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#FF0101' }}>3. Poções de Ataque:</span></p>
-<pre className="pre-p-sobre-descricao-titulo"> {`
+                    <p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#FF0101' }}>3. Poções de Ataque:</span></p>
+                    <pre className="pre-p-sobre-descricao-titulo"> {`
 Para aqueles que buscam potencializar seus ataques e derrotar inimigos com facilidade, nossas Poções de Ataque são a escolha ideal. Aqui estão algumas opções populares:
 Poção de Força Potencializada: Esta poção temporariamente aumenta sua força, permitindo-lhe causar danos devastadores em combate.
 Poção de Agilidade Felina: Torne-se tão ágil quanto um felino, atacando e se movendo com graça e rapidez.
 
-`}</pre>            
-<p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#FFE600' }}>4. Poções de Efeitos Diversos:</span></p>
-<pre className="pre-p-sobre-descricao-titulo"> {`
+`}</pre>
+                    <p className="pre-p-sobre-descricao-titulo"><span style={{ color: '#FFE600' }}>4. Poções de Efeitos Diversos:</span></p>
+                    <pre className="pre-p-sobre-descricao-titulo"> {`
 Nossa seleção de Poções de Efeitos Diversos é perfeita para experimentar efeitos mágicos únicos e inusitados. Aqui estão algumas das opções mais interessantes:
 Poção da Invisibilidade: Torne-se invisível por um curto período de tempo, permitindo que você se mova furtivamente ou evite inimigos.
 Poção de Comunicação com Animais: Abra um canal de comunicação com animais, permitindo que você compreenda e converse com eles.

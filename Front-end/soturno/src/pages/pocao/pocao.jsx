5d5fe673ab8394c7073763e438ffc5cd7cd57bb0
Leonardo Ravanelli <leonardo.lrc17@gmail.com>
2023-11-22 21:13:40 -0300

    Mudança na página Poção

--- Front-end/soturno/src/pages/pocao/pocao.jsx
+++ Front-end/soturno/src/pages/pocao/pocao.jsx
@@ -1,61 +1,114 @@
-import './pocao.scss'
+import './pocao.scss';
 import CardPocao from '../../components/card-pocao/CardPocao.jsx';
 import Header from '../../components/header/Header';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
 export default function Pocao() {
-
-    const [pocao , setPocao] = useState([])
+    const [pocao, setPocao] = useState([]);
 
     useEffect(() => {
         const buscarPocoes = async () => {
             try {
-                const resposta = await axios.get(process.env.REACT_APP_BACKEND_URL + '/pocao')
-                setPocao(resposta.data)
+                const resposta = await axios.get(process.env.REACT_APP_BACKEND_URL + '/pocao');
+                setPocao(resposta.data);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-            
+        };
+        buscarPocoes();
+    }, []);
+
+    const cardsCura = [];
+    const cardsAtaque = [];
+    const cardsDefesa = [];
+    const cardsOutros = [];
+
+    pocao.forEach((item) => {
+        const tipoLowerCase = item.tipo.toLowerCase();
+
+        switch (tipoLowerCase) {
+            case 'cura':
+                cardsCura.push(
+                    <CardPocao
+                        key={item.nome}
+                        nome={item.nome}
+                        descricao={item.descricao}
+                        imagem={item.imagem}
+                        preco={item.preco}
+                        criador={item.criador}
+                        tipo={item.tipo}
+                        ingredientes={item.ingredientes}
+                    />
+                );
+                break;
+            case 'ataque':
+                cardsAtaque.push(
+                    <CardPocao
+                        key={item.nome}
+                        nome={item.nome}
+                        descricao={item.descricao}
+                        imagem={item.imagem}
+                        preco={item.preco}
+                        criador={item.criador}
+                        tipo={item.tipo}
+                        ingredientes={item.ingredientes}
+                    />
+                );
+                break;
+            case 'defesa':
+                cardsDefesa.push(
+                    <CardPocao
+                        key={item.nome}
+                        nome={item.nome}
+                        descricao={item.descricao}
+                        imagem={item.imagem}
+                        preco={item.preco}
+                        criador={item.criador}
+                        tipo={item.tipo}
+                        ingredientes={item.ingredientes}
+                    />
+                );
+                break;
+            default:
+                cardsOutros.push(
+                    <CardPocao
+                        key={item.nome}
+                        nome={item.nome}
+                        descricao={item.descricao}
+                        imagem={item.imagem}
+                        preco={item.preco}
+                        criador={item.criador}
+                        tipo={item.tipo}
+                        ingredientes={item.ingredientes}
+                    />
+                );
+                break;
         }
-        buscarPocoes()
-    }, [])
+    });
 
     return (
-
         <div className="pagina-pocao">
-
             <section className="pocao">
-
                 <Header />
 
                 <h1 className="pocao">Cura</h1>
 
-                    <main className="pocao">
+                <div className='pocao-cards'>{cardsCura}</div>
 
+                <h1 className="pocao">Ataque</h1>
 
-                        {pocao.map((item) => {
-                            return (
-                                <CardPocao
-                                    nome={item.nome}
-                                    descricao={item.descricao}
-                                    imagem={item.imagem}
-                                    preco={item.preco}
-                                    criador={item.criador}
-                                    tipo={item.tipo}
-                                    ingredientes={item.ingredientes}
-                                />
-                            )
-                        })}
-                        
+                <div className='pocao-cards'>{cardsAtaque}</div>
+                
+                <h1 className="pocao">Defesa</h1>
 
-                    </main>
+                <div className='pocao-cards'>{cardsDefesa}</div>
 
+                <h1 className="pocao">Outros</h1>
+
+                <div className='pocao-cards'>{cardsOutros}</div>
+                
             </section>
-
         </div>
-
-    )
-
+    );
 }
